f8870d2c44633b9139ca61c76a732e28a75da1f6
Shirish Kadam <shirishkadam35@gmail.com>
2016-12-03 00:14:46 +0530

    Dependency Tokens

--- src/adam_main.py
+++ src/adam_main.py
@@ -1,5 +1,6 @@
 import spacy
 import time
+from nltk import Tree
 
 
 def get_pos_tags(sent):
@@ -21,8 +22,26 @@ def get_named_entity(sent):
     entity_set = []
     for token in sent:
         if token.ent_type != 0:
-            entity_set.append(token.text + "/" + str(token.ent_type_))
+            entity_set.append(token.text + "/" + token.ent_type_)
     return entity_set
+
+
+def get_dependency(sent):
+    dependency_set = []
+    for token in sent:
+        dependency_set.append(token.text + "/" + token.dep_)
+    return dependency_set
+
+
+def tok_format_dep(tok):
+    return "_".join([tok.orth_, tok.dep_])
+
+
+def to_nltk_tree_dep(node):
+    if node.n_lefts + node.n_rights > 0:
+        return Tree(tok_format_dep(node), [to_nltk_tree_dep(child) for child in node.children])
+    else:
+        return tok_format_dep(node)
 
 
 input_question = "How can there be any sin in sincere? Where is the good in goodbye Mr. Obama?"
@@ -33,6 +52,7 @@ en_nlp = spacy.load('en')
 en_doc = en_nlp(u'' + input_question)
 
 for sent in en_doc.sents:
+
     pos_tagset = get_pos_tags(sent)
     print(pos_tagset)
 
@@ -41,6 +61,11 @@ for sent in en_doc.sents:
 
     entity_set = get_named_entity(sent)
     print(entity_set)
+
+    dependency_set = get_dependency(sent)
+    print(dependency_set)
+
+    # to_nltk_tree_dep(sent.root).pretty_print()            # To print the Dependency Tree
 
 
 end_time = time.time()
