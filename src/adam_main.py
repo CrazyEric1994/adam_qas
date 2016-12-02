5c406c4b17043047665d069f70426e73b25a4ef0
Shirish Kadam <shirishkadam35@gmail.com>
2016-12-03 00:01:36 +0530

    Named Entity

--- src/adam_main.py
+++ src/adam_main.py
@@ -6,7 +6,7 @@ def get_pos_tags(sent):
     pos_tagset = []
     for token in sent:
         # pos_tagset[token.text] = token.pos_                   # Coarse Grained POS
-        pos_tagset.append(token.text + "/" + token.tag_)  # Fine Grained POS
+        pos_tagset.append(token.text + "/" + token.tag_)        # Fine Grained POS
     return pos_tagset
 
 
@@ -17,7 +17,15 @@ def get_lemma_set(sent):
     return lemma_set
 
 
-input_question = "How can there be any sin in sincere? Where is the good in goodbye?"
+def get_named_entity(sent):
+    entity_set = []
+    for token in sent:
+        if token.ent_type != 0:
+            entity_set.append(token.text + "/" + str(token.ent_type_))
+    return entity_set
+
+
+input_question = "How can there be any sin in sincere? Where is the good in goodbye Mr. Obama?"
 
 start_time = time.time()
 
@@ -26,9 +34,14 @@ en_doc = en_nlp(u'' + input_question)
 
 for sent in en_doc.sents:
     pos_tagset = get_pos_tags(sent)
-    lemma_set = get_lemma_set(sent)
     print(pos_tagset)
+
+    lemma_set = get_lemma_set(sent)
     print(lemma_set)
+
+    entity_set = get_named_entity(sent)
+    print(entity_set)
+
 
 end_time = time.time()
 print("Time: ", end_time - start_time)
