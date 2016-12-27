5ae366794c971dbc76665affa49a236102c28dab
Amit Gunjal <amitgunjal26@gmail.com>
2016-12-27 21:01:52 +0530

    Now check the Scikit learn file what is causing problem!

--- src/Tf-Idf/sklearn_tfidf.py
+++ src/Tf-Idf/sklearn_tfidf.py
@@ -7,27 +7,22 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 
 
-
 start_time = time.time()
 documents = []
 corpus_path = os.path.dirname(os.path.realpath(__file__))
 corpus_path = corpus_path.replace("/src/Tf-Idf", "/corpora/")
 path = corpus_path+'clustering_docs/'
 
-class amit:
 
-    def fun(self):
-        for filename in os.listdir(path):
-            documents.append(filename)
-        for i in documents:
-            with open(path+i, 'r') as text_file:
-                text = text_file.read()
-                lowers = text.lower()
-                #print(text)
-                text = text.split(".")
-                print(text)
-                new_term_freq_matrix = TfidfVectorizer.transform(self,raw_documents=text,copy=True)
-                print (TfidfVectorizer.vocabulary_)
-                print (new_term_freq_matrix.todense())
-
-amit().fun()
+for filename in os.listdir(path):
+    documents.append(filename)
+for i in documents:
+    with open(path+i, 'r') as text_file:
+        text = text_file.read()
+        lowers = text.lower()
+        #print(text)
+        #text = text.split(".")
+        #print(text)
+        new_term_freq_matrix = TfidfVectorizer.transform(raw_documents=lowers, copy=True)
+        print (TfidfVectorizer.vocabulary_)
+        print (new_term_freq_matrix.todense())
