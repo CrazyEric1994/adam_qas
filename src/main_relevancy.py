d36e2f55700565622fb757a9a020ff892f70a5e1
Vikram Abhang <abhangv@gmail.com>
2017-02-10 00:52:57 +0530

    Document pre-processing and Improved accuracy of Document relevancy using stemming

--- src/main_relevancy.py
+++ src/main_relevancy.py
@@ -1,78 +1,87 @@
-#------------------------Relevant Document Retrieval -------------------------
-#---------------------------Imports---------------------------------------
-import nltk
+
 import os
-from nltk import word_tokenize
-#----------------------------------Class for TF- IDF --------------------
-class tfidf:
-  def __init__(self):
-    self.weighted = False
-    self.documents = []
-    self.corpus_dict = {}
+import nltk
+from nltk.stem.snowball import SnowballStemmer
+import re
+from nltk.corpus import stopwords
 
-  def addDocument(self, doc_name, list_of_words):
-    # building a dictionary
-    doc_dict = {}
-    for w in list_of_words:
-      doc_dict[w] = doc_dict.get(w, 0.) + 1.0
-      self.corpus_dict[w] = self.corpus_dict.get(w, 0.0) + 1.0
+stemmer = SnowballStemmer("english")                                            # Applied Stemming
 
-    # normalizing the dictionary
-    length = float(len(list_of_words))
-    for k in doc_dict:
-      doc_dict[k] = doc_dict[k] / length
 
-    # add the normalized document to the corpus
-    self.documents.append([doc_name, doc_dict])
+class TfIdf:
+      def __init__(self):
+        self.weighted = False
+        self.documents = []
+        self.corpus_dict = {}
 
-  def similarities(self, list_of_words):
-  # building the query dictionary
-    query_dict = {}
-    for w in list_of_words:
-      query_dict[w] = query_dict.get(w, 0.0) + 1.0
+      def addDocument(self,  doc_name,  list_of_words):                      # building a dictionary
+        doc_dict = {}
+        for w in list_of_words:
+            doc_dict[w] = doc_dict.get(w,  0.) + 1.0
+            self.corpus_dict[w] = self.corpus_dict.get(w,  0.0) + 1.0
 
-    # normalizing the query
-    length = float(len(list_of_words))
-    for k in query_dict:
-      query_dict[k] = query_dict[k] / length
+        length = float(len(list_of_words))                                  # normalizing the dictionary
+        for k in doc_dict:
+            doc_dict[k] = (doc_dict[k] / length)
+        self.documents.append([doc_name,  doc_dict])                        # add the normalized document to the corpus
 
-    # computing the list of similarities
-    sims = []
-    for doc in self.documents:
-      score = 0.0
-      doc_dict = doc[1]
-      for k in query_dict:
-        if k in doc_dict:
-          score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
-      sims.append([doc[0], score])
+      def similarities(self,  list_of_words):                               # building the query dictionary
+        query_dict = {}
+        for w in list_of_words:
+            query_dict[w] = query_dict.get(w,  0.0) + 1.0
 
-    return sims
-#---------------------------End of Class-----------------------------------------------------------------------------
+        # normalizing the query
+        length = float(len(list_of_words))
+        for k in query_dict:
+            query_dict[k] = (query_dict[k] / length)
 
-#---------------------Temporary Data structure----------------------------------------------------------------
-documents = ['barack.txt','elon.txt','linus.txt']
-file_content = [1,2,3]
-tokens = [1,2,3]
-titles = ['Barack Obama','Elon Musk','Linus Torvalds']
+        # computing the list of similarities
+        sims = []
+        for doc in self.documents:
+            score = 0.0
+            doc_dict = doc[1]
+            for k in query_dict:
+              if k in doc_dict:
+                score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
+            sims.append([doc[0],  score])
+
+        return sims
+
+documents = ['barack.txt', 'elon.txt', 'linus.txt']
+file_content = [1, 2, 3]
+tokens = file_content
+titles = ['Barack Obama', 'Elon Musk', 'Linus Torvalds']
 doc_len = len(documents)
 key = []
+filtered = []
+stemmed = []
 
-#--------------------Ask Question-----------------------------------------------------------------------------------
-#------------------this is temporary cause we need keywords----------------------------------------------
 
 q = input("Q.").strip()
-q=q.lower()
+q = q.lower()
 q_tok = nltk.word_tokenize(q)
-print(q_tok)
-#---------------------------------------------Object of class-----------------------------------------------------
-obj = tfidf()
+question_stemmed = []
+for item in q_tok:
+    question_stemmed.append(stemmer.stem(item))
+print(question_stemmed)
+
+obj = TfIdf()
 
 path = os.path.dirname(os.path.realpath(__file__))
-path = path.replace("/src", "/corpora/")
+path = path.replace("/src",  "/corpora/")
+pattern = '([[])\w+]'
 
-for i in range(0,doc_len):
-	file_content[i] = open(path+documents[i]).read()
-	file_content[i] = file_content[i].lower()
-	tokens[i] = nltk.word_tokenize(file_content[i])
-	obj.addDocument(titles[i],tokens[i])
-print(obj.similarities(q_tok))
+for i in range(0, doc_len):
+    file_content[i] = open(path+documents[i]).read()                                # read content
+    file_content[i] = file_content[i].lower()                                       # lower them
+    file_content[i] = re.sub(pattern, '', file_content[i])                          # remove citations
+    tokens[i] = nltk.word_tokenize(file_content[i])                                 # tokenize the document
+    filtered = [w for w in tokens[i] if not w in stopwords.words('english')]        # remove stopwords
+    # print(filtered)                                                                 # print tokens
+    for item in filtered:
+        stemmed.append(stemmer.stem(item))
+    # print(stemmed)
+    obj.addDocument(titles[i], stemmed)                                           # sent it to similarity checking
+
+print(obj.similarities(question_stemmed))
+
