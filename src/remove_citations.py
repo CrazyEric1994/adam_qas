4e0dd8d12800babfd4316abde26e95cefb13d755
Amit Gunjal <amitgunjal26@gmail.com>
2017-03-11 12:33:29 +0530

    Remove citations from wikipedia text files

---
+++ src/remove_citations.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+path = os.path.dirname(os.path.realpath(__file__))
+path = path.replace("/src",  "/corpora/elon.txt")
+
+pattern = '([[])\w+]'
+with open(path, 'r') as f:
+    lines = f.read()
+
+
+def remove_Citations():
+    for line in lines:
+        match = re.search(pattern, line)
+        if match:
+            new_line = match.group() + '\n'
+    removed_citations = re.sub(pattern, '', lines)
+    return removed_citations
+
+print(remove_Citations())
