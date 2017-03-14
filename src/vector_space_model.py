ef0d38529d3365a27ce19b4b039586e62f86ed6d
Amit Gunjal <amitgunjal26@gmail.com>
2017-03-15 00:16:51 +0530

    extract documents from wikipedia based on search term.

--- src/vector_space_model.py
+++ src/vector_space_model.py
@@ -9,12 +9,12 @@ stemmer = SnowballStemmer("english")                                            
 
 
 class TfIdf:
-      def __init__(self):
+    def __init__(self):
         self.weighted = False
         self.documents = []
         self.corpus_dict = {}
 
-      def addDocument(self,  doc_name,  list_of_words):                      # building a dictionary
+    def add_document(self,  doc_name,  list_of_words):                      # building a dictionary
         doc_dict = {}
         for w in list_of_words:
             doc_dict[w] = doc_dict.get(w,  0.) + 1.0
@@ -25,7 +25,7 @@ class TfIdf:
             doc_dict[k] = (doc_dict[k] / length)
         self.documents.append([doc_name,  doc_dict])                        # add the normalized document to the corpus
 
-      def similarities(self,  list_of_words):                               # building the query dictionary
+    def similarities(self,  list_of_words):                               # building the query dictionary
         query_dict = {}
         for w in list_of_words:
             query_dict[w] = query_dict.get(w,  0.0) + 1.0
@@ -41,18 +41,16 @@ class TfIdf:
             score = 0.0
             doc_dict = doc[1]
             for k in query_dict:
-              if k in doc_dict:
-                score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
+                if k in doc_dict:
+                    score += (query_dict[k] / self.corpus_dict[k]) + (doc_dict[k] / self.corpus_dict[k])
             sims.append([doc[0],  score])
 
         return sims
 
-documents = ['p1.txt', 'p2.txt', 'p3.txt', 'p4.txt',
-             'p5.txt']                 # Temporary Data Structures
-file_content = [1, 2, 3, 4, 5 ]
-tokens = [1, 2, 3, 4, 5 ]
-
-titles = ['Barack Obama', 'Donald Trump', 'Narendra Modi', 'Sharad Pawar', 'Vladimir Putin']
+documents = ['p1.txt', 'p2.txt', 'p3.txt', 'p4.txt', 'p5.txt']                 # Temporary Data Structures
+file_content = [1, 2, 3, 4, 5]
+tokens = file_content
+titles = ['Barack H Obama', 'Donald J Trump', 'Narendra D Modi', 'Sharad Pawar', 'Vladimir Putin ']
 doc_len = len(documents)
 key = []
 filtered = []
@@ -64,13 +62,13 @@ q = q.lower()
 q_tok = nltk.word_tokenize(q)                                          # Tokenizing it
 question_stemmed = []
 for item in q_tok:
-    question_stemmed.append(stemmer.stem(item))                        # Stemming it
+    question_stemmed.append(stemmer.stem(item))                        # Stem it
 print(question_stemmed)
 
 obj = TfIdf()                                                           # Object of Class
 
 path = os.path.dirname(os.path.realpath(__file__))
-path = path.replace("/src",  "/corpora/clustering_docs/")
+path = path.replace("/src",  "/corpora/clustering_data/")
 pattern = '([[])\w+]'
 
 for i in range(0, doc_len):
@@ -79,11 +77,10 @@ for i in range(0, doc_len):
     file_content[i] = re.sub(pattern, '', file_content[i])                          # remove citations
     tokens[i] = nltk.word_tokenize(file_content[i])                                 # tokenize the document
     filtered = [w for w in tokens[i] if not w in stopwords.words('english')]        # remove stopwords
-    # print('Remove Stopwords :', filtered)                                                               # print tokens
+    # print(filtered)                                                               # print tokens
     for item in filtered:
         stemmed.append(stemmer.stem(item))
-    # print('Stemmed :', stemmed)
-    obj.addDocument(titles[i], stemmed)                                           # sent it to similarity checking
+    # print(stemmed)
+    obj.add_document(titles[i], stemmed)                                           # sent it to similarity checking
 
 print(obj.similarities(question_stemmed))
-
